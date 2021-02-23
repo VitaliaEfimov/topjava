70569cc4a6bcdc1f05096c34dd0efd766d5edc8d
Виталя <footballervit@mail.ru>
2021-02-23 21:35:45 +0800

    HW03

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,25 +1,37 @@
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS global_seq;
+DROP TABLE IF EXISTS user_roles CASCADE ;
+DROP TABLE IF EXISTS users CASCADE ;
+DROP TABLE IF EXISTS meals CASCADE ;
+DROP SEQUENCE IF EXISTS global_seq CASCADE ;
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
 CREATE TABLE users
 (
     id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    name             VARCHAR                           NOT NULL,
-    email            VARCHAR                           NOT NULL,
-    password         VARCHAR                           NOT NULL,
-    registered       TIMESTAMP           DEFAULT now() NOT NULL,
-    enabled          BOOL                DEFAULT TRUE  NOT NULL,
-    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
+    name             VARCHAR                 NOT NULL,
+    email            VARCHAR                 NOT NULL,
+    password         VARCHAR                 NOT NULL,
+    registered       TIMESTAMP DEFAULT now() NOT NULL,
+    enabled          BOOL DEFAULT TRUE       NOT NULL,
+    calories_per_day INTEGER DEFAULT 2000    NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_index ON users (email);
 
 CREATE TABLE user_roles
 (
     user_id INTEGER NOT NULL,
     role    VARCHAR,
-    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    CONSTRAINT user_roles_index UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id     INTEGER         NOT NULL,
+    date_time   TIMESTAMP       NOT NULL,
+    description VARCHAR(255)    NOT NULL,
+    calories    INTEGER         NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ,
+    CONSTRAINT meal_date_time UNIQUE (user_id, date_time)
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,33 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+TRUNCATE user_roles;
+TRUNCATE meals;
+TRUNCATE users CASCADE;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password)
-VALUES ('User', 'user@yandex.ru', 'password'),
-       ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password) VALUES
+('User', 'user@yandex.ru', 'password'),
+('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id)
-VALUES ('USER', 100000),
-       ('ADMIN', 100001);
+INSERT INTO user_roles (role, user_id) VALUES
+('ROLE_USER', 100000),
+('ROLE_ADMIN', 100001);
+
+insert into meals (user_id,date_time,description,calories)
+select t.user_id, t.date_time, t.description, t.calories from
+    (select 1 ord, 100000 user_id, to_timestamp('30/01/2020 10:00','dd/mm/yyyy hh24:mi') date_time, 'Завтрак' description, 500 calories
+     union
+     select 2 ord, 100000 user_id, to_timestamp('30/01/2020 13:00','dd/mm/yyyy hh24:mi') date_time, 'Обед' description, 1000 calories
+     union
+     select 3 ord, 100000 user_id, to_timestamp('30/01/2020 20:00','dd/mm/yyyy hh24:mi') date_time, 'Ужин' description, 500 calories
+     union
+     select 4 ord, 100000 user_id, to_timestamp('31/01/2020 00:00','dd/mm/yyyy hh24:mi') date_time, 'Еда на ограничение' description, 100 calories
+     union
+     select 5 ord, 100000 user_id, to_timestamp('31/01/2020 10:00','dd/mm/yyyy hh24:mi') date_time, 'Завтрак' description, 1000 calories
+     union
+     select 6 ord, 100000 user_id, to_timestamp('31/01/2020 13:00','dd/mm/yyyy hh24:mi') date_time, 'Обед' description, 500 calories
+     union
+     select 7 ord, 100000 user_id, to_timestamp('31/01/2020 20:00','dd/mm/yyyy hh24:mi') date_time, 'Ужин' description, 410 calories) t
+order by t.ord;
+
+SELECT * FROM meals;
+SELECT * FROM user_roles;
+SELECT * FROM users;
